323db78f70e603c1142c67f74da067193829c6a1
madworksrocks <mohitmohandas1@gmail.com>
2020-05-13 13:50:59 +0530

    Worked on worldgen module for basic functionality.

--- modules/worldgen.py
+++ modules/worldgen.py
@@ -1,15 +1,24 @@
-
+import numpy as np
+import json
 
 class Main:
-
+    world_shape = (100,100,100)
+    tile_size = 11
+    mmap_mode = None
     def __init__(self, main):
         self.main = main
 
     def create(self, wnf, wkf):
-        pass
+        self.main.worldnp = np.chararray(self.world_shape, itemsize=self.tile_size)
+        self.main.worldkeys = {}
+
+        np.save(wnf, self.main.worldnp, allow_pickle=False)
+        json.dump(self.main.worldkeys, wkf)
 
     def load(self, wnf, wkf):
-        pass
+        self.main.worldnp = np.load(wnf, mmap_mode=self.mmap_mode, allow_pickle=False)
+        self.main.worldkeys = json.load(wkf)
 
     def save(self, wnf, wkf):
-        pass
+        np.save(wnf, self.main.worldnp, allow_pickle=False)
+        json.dump(self.main.worldkeys, wkf)
