3d06887df7249295274730d5205b0e099eac8845
madworksrocks <mohitmohandas1@gmail.com>
2021-01-30 14:16:08 +0530

    Scaffolding.

---
+++ README.md
@@ -0,0 +1,2 @@
+# AutoColony
+One of a kind scripting open world simulator.
---
+++ main.py
@@ -0,0 +1,20 @@
+
+
+class Main:
+    
+    def __init__(self):
+        pass
+
+    def tick(self):
+        pass
+
+    def quit(self):
+        pass
+
+
+isRunning = False
+if __name__ == "__main__":
+    main = Main()
+
+    while isRunning:
+        main.tick()
